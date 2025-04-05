d9b26338a7f0110e9c6f1def12e77ede5e6f256d
wilardplayz <wilardplayz@gmail.com>
2025-04-05 13:39:11 +0000

    im dying

--- next.config.js
+++ next.config.js
@@ -39,9 +39,6 @@ const nextConfig = {
     typescript: {
       ignoreBuildErrors: true,
     },
-    experimental: {
-      esmExternals: 'loose'
-    },
     eslint: {
       ignoreDuringBuilds: true,
     },
